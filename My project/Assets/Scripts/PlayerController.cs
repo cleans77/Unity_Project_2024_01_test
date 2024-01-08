525c8a3872cdb9648ba2a0a98c28f2e013962421
cleans77 <sdh230202@sdh.hs.kr>
2024-01-08 14:15:16 +0900

    탱크 움직임 + 빵야빠야

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public GameObject pivot;
     public Camera viewCamera; //main Camera Object
     public Vector3 velocity;
+    public Rigidbody body;
     void Start()
     {
         viewCamera = Camera.main;
@@ -16,6 +17,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * moveSpeed;
         //화면에서 -> 게임 3D 공간 좌표를 변환해서 Vector3에 넣는다.
         Vector3 mousePos = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
             Input.mousePosition.y,viewCamera.transform.position.y));
@@ -23,5 +25,9 @@ public class PlayerController : MonoBehaviour
         Vector3 targetPos = new Vector3(mousePos.x,pivot.transform.position.y,mousePos.z);
 
         pivot.transform.LookAt(targetPos, Vector3.up);
+    }
+    private void FixedUpdate()
+    {
+        body.MovePosition(body.position + velocity * Time.fixedDeltaTime);
     }
 }
